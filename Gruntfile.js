1b1ff80e8a717faeb7f151f3bfa1004a0b3acf19
fancy <fancyboynet@gmail.com>
2014-09-03 19:24:42 +0800

    发布版本v0.0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,101 @@
 module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buildPath : 'build',
+        buildCompressPath : 'build/tmp',
+        outputPath : '../show-built',
+        testPath : 'tests',
+        jshint : {
+            options : {
+                sub : true,
+                ignores : ['node_modules/**/*.js', 'bower_components/**/*.js']
+            },
+            all : ['**/*.js', '!Gruntfile.js']
+        },
+        clean: {
+            options : {
+                force : true
+            },
+            build : ["<%= buildPath %>"],
+            output : ["<%= outputPath %>", "!<%= buildPath %>/.git"]
+        },
+        copy : {
+            build : {
+                files : [
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        src: [
+                            '**/*',
+                            '!.**/*',
+                            '!Gruntfile.js',
+                            '!bower.json',
+                            '!<%= testPath %>/**/*',
+                            '!node_modules/**/*',
+                            '!bower_components/**/*',
+                            '!**.md',
+                            '!**.iml'
+                        ],
+                        dest: '<%= buildPath %>/'
+                    },
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        src: [
+                            'bower_components/bootstrap/dist/**/*',
+                            'bower_components/ejs/ejs.min.js',
+                            'bower_components/jquery/dist/jquery.min.js',
+                            'bower_components/requirejs/require.js'
+                        ],
+                        dest: '<%= buildPath %>/'
+                    }
+                ]
+            },
+            output : {
+                files : [
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        cwd : '<%= buildCompressPath %>/',
+                        src: [
+                            '**/*'
+                        ],
+                        dest: '<%= outputPath %>/'
+                    }
+                ]
+            }
+        },
+        requirejs: {
+            build: {
+                options: {
+                    appDir : '<%= buildPath %>',
+                    dir: '<%= buildCompressPath %>',
+                    mainConfigFile: "static/lib/requireConfig.js",
+                    baseUrl: "./",
+                    paths: {
+                        "jquery": "empty:",
+                        "bootstrap": "empty:"
+                    },
+                    modules : [
+                        {
+                            name: "static/page/index"
+                        },
+                        {
+                            name: "static/page/error"
+                        }
+                    ],
+                    optimizeCss: "standard"
+                }
+            }
+        }
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('output', ['clean:output', 'copy:output', 'clean:build']);
+    grunt.registerTask('build', ['jshint', 'clean:build', 'copy:build', 'requirejs:build', 'output']);
 };
--- controllers/data/works.js
+++ controllers/data/works.js
@@ -7,11 +7,14 @@ var getWorksBetween = function(range, callback){
     var start = range.range[0];
     var end = range.range[1];
     Works.find().skip(start).limit(end - start + 1).lean().exec(function(err, works){
-        if (err) {
-            callback && callback(err);
+        if(!callback){
             return;
         }
-        callback && callback(null, {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, {
             totalPages : range.totalPages,
             works : works
         });
@@ -19,13 +22,16 @@ var getWorksBetween = function(range, callback){
 };
 var getWorksByPage = function(page, pageRecords, callback){
     Works.count(function(err, count){
+        if(!callback){
+            return;
+        }
         if (err) {
-            callback && callback(err);
+            callback(err);
             return;
         }
         var range = util.getPageRange(count, pageRecords, page);
         if(page > range.totalPages){
-            callback && callback(null, {
+            callback(null, {
                 totalPages : range.totalPages,
                 works : []
             });
@@ -45,8 +51,11 @@ module.exports = {
     },
     add : function(data, callback){
         var work = new Works(data);
+        if(!callback){
+            return;
+        }
         work.save(function(err){
-            callback && callback(err);
+            callback(err);
         });
     }
 };
